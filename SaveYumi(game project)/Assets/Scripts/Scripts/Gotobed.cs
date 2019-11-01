af62226bde0aeb0f6d8ec2c88686ad0e6f7aea6f
Stone Eskelsen <20.stone.eskelsen@ksd.org>
2019-11-01 10:24:27 -0700

    Enemy Sword attack + Animation AC

---
+++ SaveYumi(game project)/Assets/Scripts/Scripts/Gotobed.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gotobed : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                GetComponentInChildren<Canvas>().enabled = true;
+
+            }
+            else
+            {
+                Time.timeScale = 1;
+                GetComponentInChildren<Canvas>().enabled = false;
+            }
+           
+        }
+    }   
+    public void No()
+    {
+        Time.timeScale = 1;
+        GetComponentInChildren<Canvas>().enabled = false;
+    }
+    public void Yes()
+    {
+     
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Good Ending");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
